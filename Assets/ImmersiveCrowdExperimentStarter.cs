e26cc3032601e80ea0cc22e43a0e287284ebf091
Daniele Giunchi <dannox@gmail.com>
2020-08-17 10:37:04 +0100

    add a simple interface during intro, it can be customized and with a static class between the scenes some parameters can be passed

--- Assets/ImmersiveCrowdExperimentStarter.cs
+++ Assets/ImmersiveCrowdExperimentStarter.cs
@@ -14,8 +14,9 @@ public class ImmersiveCrowdExperimentStarter : MonoBehaviour
 
     void OnGUI()
     {
-        if (GUI.Button(new Rect(20, 30, 150, 30), "To the simulation... we can use here a staticclass for cross information activated by specific buttons in an interface"))
+        if (GUI.Button(new Rect(20, 30, 150, 30), "To the simulation..."))
         {
+            // we can use here a staticclass for cross information activated by specific buttons in an interface
             SceneManager.LoadScene("sample_network_scene", LoadSceneMode.Single);
         }
     }
@@ -24,9 +25,12 @@ public class ImmersiveCrowdExperimentStarter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.Get(OVRInput.Button.One))
-        {
-            SceneManager.LoadScene("sample_network_scene", LoadSceneMode.Single);
-        }
+        
     }
+
+    public void LoadSimulation()
+    {
+        SceneManager.LoadScene("sample_network_scene", LoadSceneMode.Single);
+    }
+
 }
