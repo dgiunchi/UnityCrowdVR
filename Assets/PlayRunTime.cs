f3e94bf0e2151e05806590f708d1f15611de90a7
Daniele Giunchi <dannox@gmail.com>
2020-11-22 14:00:11 +0000

    ad test for playvideo

---
+++ Assets/PlayRunTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class PlayRunTime : MonoBehaviour
+{
+    bool triggerPlay = false;
+    private VideoPlayer MyVideoPlayer;
+
+    public void PlayVideo()
+    {
+        MyVideoPlayer = GetComponent<VideoPlayer>();
+        // play video player
+        MyVideoPlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetKeyDown(KeyCode.P) || OVRInput.Get(OVRInput.Button.Three) && triggerPlay == false)
+        {
+            PlayVideo();
+            triggerPlay = true;
+        }
+    }
+}
