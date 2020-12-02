c326967b4182d4920018743605735e69c3e2e5dc
Riccardo Bovo <rb1619@ic.ac.uk>
2020-12-02 12:34:49 +0000

    setinitial position fix

--- Assets/SetInitialPosition.cs
+++ Assets/SetInitialPosition.cs
@@ -4,30 +4,89 @@ using UnityEngine;
 
 public class SetInitialPosition : MonoBehaviour
 {
+    Transform target;
+    Transform forward;
+    Transform floor;
+    GameObject ui;
+    Bounds bounds;
+    OVRPlayerController ovrpc;
+    CharacterController cc;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        
+        target = GameObject.Find("OVRPlayerController").transform;
+        forward = target.FindDeepChild("ForwardDirection").transform;
+        floor = GameObject.Find("Ground").transform;      
+        ovrpc = target.GetComponent<OVRPlayerController>();
+        cc = target.GetComponent<CharacterController>();
+
+        ovrpc.GravityModifier = 0;
+
+
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        bounds = new Bounds(transform.position, Vector3.one);
+        foreach (Renderer r in renderers)
+        {
+            bounds.Encapsulate(r.bounds);
+        }
+        
         SetPlayerInitialPosition();
     }
 
     void SetPlayerInitialPosition()
     {
-        Collider[] colliders = GetComponentsInChildren<Collider>();
-        Bounds bounds = new Bounds(transform.position, Vector3.one);
-        foreach (Collider c in colliders)
-        {
-            bounds.Encapsulate(c.bounds);
-        }
-        Transform target = GameObject.Find("OVRPlayerController").transform;
-        Transform floor = GameObject.Find("Ground").transform;
-        target.position = new Vector3(bounds.center.x, floor.position.y + 2.0f, bounds.center.z);
-        Transform ui = GameObject.Find("EndOfTrialManager").transform;
-        ui.position = new Vector3(target.position.x, target.position.y, target.position.z + 3f);
+
+        target.position = new Vector3(bounds.center.x, floor.position.y + 1.8f, bounds.center.z);
+        target.rotation = Quaternion.identity;
+
+        //if (ui != null)
+        //{
+        //    ui.transform.position = forward.transform.position;
+        //    ui.transform.rotation = forward.transform.rotation;
+        //    ui.transform.Translate(forward.transform.forward * 2f);
+        //}
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (target.position.y < 0)
+        {
+            ovrpc.GravityModifier = 0;
+          
+            SetPlayerInitialPosition();
+
+        }
+        else {
+
+            ovrpc.GravityModifier = 1;
+            
+        }
     }
+
+    //private void Update()
+    //{
+    //    if (ui == null)
+    //    {
+    //        getUi();
+    //    }
+
+        
+    //}
+
+    //void getUi() {
+        
+    //    if (ui == null) { 
+
+    //        ui = GameObject.Find("EndOfTrialManager");
+    //    }
+        
+    //    if (ui == null)
+    //    {
+    //        ui = GameObject.Find("Questionaire Panel");
+    //    }
+
+    //}
 }
